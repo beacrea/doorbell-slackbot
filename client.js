a5b762d5f824c7197107cd80af1866d5ee236897
Coty Beasley <coty@cotybeasley.name>
2017-04-15 13:22:01 -0700

    Added more audio files

--- client.js
+++ client.js
@@ -34,7 +34,7 @@ Client.prototype = {
 
     // afplay for macs
     // mplayer for linux
-    const playerChildProcess = spawn('mplayer', [filePath]);
+    const playerChildProcess = spawn('afplay', [filePath]);
     playerChildProcess.on("end", () => {
       console.log("Done playing " + filePath);
     });
