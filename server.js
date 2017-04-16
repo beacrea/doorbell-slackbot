3cc6681742e8b84106e1e5d358a417c1f63ee6c7
Coty Beasley <coty@cotybeasley.name>
2017-04-15 20:33:13 -0700

    Updated server.js to be strict

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 
@@ -5,27 +7,27 @@ const SERVER_PORT = 1234;
 let doorBellActive = false;
 
 const FILES = {
-  PASS: "meseeks2.mp3",
-  FAIL: "inception.mp3"
+    PASS: "meseeks2.mp3",
+    FAIL: "inception.mp3"
 };
 
 app.get('/', function (req, res) {
-  if (doorBellActive === true) {
-    res.send(FILES.PASS);
-  }
+    if (doorBellActive === true) {
+        res.send(FILES.PASS);
+    }
 });
 
 app.post('/', function (req, res) {
-  doorBellActive = true;
-  console.log('Doorbell status: ' + doorBellActive);
-  res.send("You sent a doorbell notification!");
-  setTimeout(function() {
-    doorBellActive = false;
+    doorBellActive = true;
     console.log('Doorbell status: ' + doorBellActive);
-  }, 10000);
+    res.send("You sent a doorbell notification!");
+    setTimeout(function() {
+        doorBellActive = false;
+        console.log('Doorbell status: ' + doorBellActive);
+    }, 10000);
 });
 
 app.listen(SERVER_PORT, function () {
-  console.log('Example app listening on port ' + SERVER_PORT + '!');
-  console.log('Doorbell status: ' + doorBellActive + '\n');
+    console.log('Example app listening on port ' + SERVER_PORT + '!');
+    console.log('Doorbell status: ' + doorBellActive + '\n');
 });
